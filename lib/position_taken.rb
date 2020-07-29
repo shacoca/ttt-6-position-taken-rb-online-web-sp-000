ec801ce9e6e425b7e7ac25a38308a60198d563da
shawn campbell <shacoca@yahoo.com>
2020-07-29 16:52:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == (" " || "" || nil)
+  if board[index] == (" " || "" || nil) || (index < 1 || index > 9)
     false
   elsif board[index] == ( "X" || "O" )
     true
